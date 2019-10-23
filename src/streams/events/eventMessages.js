bf23e7e89804e9906e04932556fa0ebee7840ca8
davidelden <deelden+github@gmail.com>
2019-10-23 12:30:16 -0500

    update eventMessages to pass error details and zip codes

--- src/streams/events/eventMessages.js
+++ src/streams/events/eventMessages.js
@@ -1,8 +1,16 @@
+const dataMessage = zipCode => (
+  ['message', 'WeatherPackageAvailable', 'zipcode', zipCode]
+);
+
+const errorMessage = errMsg => (
+  ['message', 'WeatherPackagerError', 'error', errMsg]
+);
+
 const eventMessages = {
   start: ['message', 'WeatherPackagerStart'],
   end: ['message', 'WeatherPackagerEnd'],
-  data: ['message', 'WeatherPackageAvailable'],
-  error: ['message', 'WeatherPackagerError']
+  data: dataMessage,
+  error: errorMessage
 }
 
 module.exports = eventMessages;
