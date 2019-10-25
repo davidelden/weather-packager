8864199ea6dd27ac32f6f0543d7ce5dd74b2883e
davidelden <deelden+github@gmail.com>
2019-10-25 12:47:03 -0500

    begin createWeatherPackage rewrite

--- src/packager/createWeatherPackage.js
+++ src/packager/createWeatherPackage.js
@@ -1,20 +1,27 @@
-const createWeatherPackage = weatherData => {
-  // Interpret weather data
-  // Create message to send to AWS SNS
-  // Return packaged message
-}
+const fetchFromAPI = require('../fetch/helpers/fetchFromAPI.js'),
+      weatherAPIEndpoint = require('../fetch/helpers/weatherAPIEndpoint.js'),
+      saveWeatherPackage = require('./helpers/saveWeatherPackage.js'),
+      writeStream = require('../streams/actions/writeStream.js'),
+      eventMessages = require('../streams/events/eventMessages.js'),
+      streamName = 'WeatherPackage';
 
-const createWeatherMessage = (msg, zipCode) => {
-  const weatherMsg = {
-    "TopicArn": process.env.AWS_SNS_TOPIC_ARN,
-    "Message": msg,
-    "MessageAttributes": {
-      "zip_codes": {
-        "DataType": "String",
-        "StringValue": zipCode
-      }
-    }
-  }
+const createWeatherPackage = async msg => {
+    if(!Array.isArray(msg) || msg[1] !== 'WeatherFetchDataAvailable') return;
+
+    writeStream(streamName, eventMessages['start']);
+
+    const zipCode = msg[3],
+          endPoint = weatherAPIEndpoint(zipCode),
+          weatherData = await fetchFromAPI(endPoint);
+
+    // Fetch weather
+    console.log('weatherData', weatherData);
+
+    // Interpret weather data
+    // Create message to send to AWS SNS
+    // Return packaged message
+
+    writeStream(streamName, eventMessages['end']);
 }
 
 module.exports = createWeatherPackage;
