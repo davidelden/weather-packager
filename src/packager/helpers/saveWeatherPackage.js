9f823f585e91fdd56b4f699e73abaa7a33435360
davidelden <deelden+github@gmail.com>
2019-10-29 13:03:32 -0500

    make saveWeatherPackage fn into upsert query

--- src/packager/helpers/saveWeatherPackage.js
+++ src/packager/helpers/saveWeatherPackage.js
@@ -1,12 +1,25 @@
-const // db = require('../../db/connection.js'),
+const util = require('util'),
+      db = require('../../db/connection.js'),
       writeStream = require('../../streams/actions/writeStream.js'),
       eventMessages = require('../../streams/events/eventMessages.js'),
-      streamName = 'WeatherFetch';
+      streamName = 'WeatherPackage';
 
-const saveWeatherPackage = (dbTbl, zipCode, data) => {
-  db(dbTbl)
-    .where({ zip_code: zipCode })
-    .update({ data: data, updated_at: db.fn.now() })
+const saveWeatherPackage = async (dbTbl, zipCode, data) => {
+  const insert = db(dbTbl)
+    .insert({ zip_code: zipCode, message: JSON.stringify(data) })
+    .toString()
+
+  const update = db(dbTbl)
+    .update({ message: JSON.stringify(data), updated_at: db.fn.now() })
+    .whereRaw(`${dbTbl}.zip_code = ?`, [zipCode])
+
+  const query = util.format(
+    '%s ON CONFLICT (zip_code) DO UPDATE SET %s',
+    insert.toString(),
+    update.toString().replace(/^update\s.*\sset\s/i, '')
+  )
+
+  db.raw(query)
     .then(() => {
       writeStream(streamName, eventMessages['data'](zipCode));
       console.log(`${dbTbl} updated with ${zipCode} weather package`);
