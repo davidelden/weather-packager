426380b6f9432537de5cc8c88613689d38a8c235
davidelden <deelden+github@gmail.com>
2019-11-04 15:57:00 -0600

    add updateWeatherPackage fn to index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const migrateLatest = require('./src/db/migrateLatest.js'),
       readStream = require('./src/streams/actions/readStream.js'),
       msgEmitter = require('./src/emitter/msgEmitter'),
       createWeatherPackage = require('./src/packager/createWeatherPackage.js'),
+      updateWeatherPackage = require('./src/packager/updateWeatherPackage.js'),
       startServer = require('./src/api/server.js'),
       streamWeatherFetch = 'WeatherFetch',
       streamWeatherSender = 'WeatherSender';
@@ -12,4 +13,5 @@ migrateLatest();
 readStream(streamWeatherFetch);
 readStream(streamWeatherSender);
 msgEmitter.on('streamMessage', msg => createWeatherPackage(msg));
+msgEmitter.on('streamMessage', msg => updateWeatherPackage(msg));
 startServer();
