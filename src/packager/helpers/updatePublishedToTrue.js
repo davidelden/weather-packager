d9880246ac2c8afad95a6d14f8ff61318bfb27ef
davidelden <deelden+github@gmail.com>
2019-11-05 11:53:00 -0600

    update timestamp when updating published

--- src/packager/helpers/updatePublishedToTrue.js
+++ src/packager/helpers/updatePublishedToTrue.js
@@ -3,7 +3,7 @@ const db = require('../../db/connection.js');
 const updatePublishedToTrue = (dbTbl, zipCode) => {
   db(dbTbl)
     .where('zip_code', zipCode)
-    .update({ published: true })
+    .update({ published: true, updated_at: db.fn.now() })
     .then(() => console.log(`${zipCode} weather package published=true`))
     .catch(err => console.error('error:', err))
 }
