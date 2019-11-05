dee8cd06aaf53137e3b617765242a923dea25796
davidelden <deelden+github@gmail.com>
2019-11-05 11:55:25 -0600

    allow reading from multiple streams

--- src/streams/actions/readStream.js
+++ src/streams/actions/readStream.js
@@ -2,29 +2,37 @@ const redis = require('redis'),
       client = redis.createClient({ host: 'redis' }),
       msgEmitter = require('../../emitter/msgEmitter.js');
 
-const readStream = (streamName, elemId = '$', timeout = '0') => {
-  let newId = elemId;
+const readStream = (streamInfoArr, timeout = '0') => {
+  const streamNames = streamInfoArr.map(stream => stream[0]),
+        elemIds = streamInfoArr.map(stream => stream[1]),
+        streamInfoArrCopy = [ ...streamInfoArr ];
 
-  client.xread(['BLOCK', timeout, 'STREAMS', streamName, elemId], (err, stream) => {
+  client.xread(['COUNT', '1', 'BLOCK', timeout, 'STREAMS', ...streamNames, ...elemIds], (err, stream) => {
     if (err) return console.error('Error reading from stream:', err);
 
     if(stream) {
-      newId = stream[0][1][0][0];
-      let streamArr = [ ...stream[0][1][0][1] ],
-          msgIndex = streamArr.indexOf('message') + 1,
-          zipCodeIndex = streamArr.indexOf('zipcode'),
-          msg = streamArr[msgIndex];
+      for(arr of stream) {
+        let streamName = arr[0],
+            streamNameIndex = streamNames.indexOf(streamName),
+            elemId = arr[1][0][0],
+            streamArr = [ ...arr[1][0][1] ],
+            msgIndex = streamArr.indexOf('message') + 1,
+            zipCodeIndex = streamArr.indexOf('zipcode'),
+            msg = streamArr[msgIndex];
 
-      if(zipCodeIndex !== -1) {
-        let zipCode = streamArr[zipCodeIndex+1];
+        if(zipCodeIndex !== -1) {
+          let zipCode = streamArr[zipCodeIndex+1];
 
-        msgEmitter.emit('streamMessage', [msg, zipCode]);
-      } else {
-        msgEmitter.emit('streamMessage', msg);
+          msgEmitter.emit('streamMessage', [msg, zipCode]);
+        } else {
+          msgEmitter.emit('streamMessage', msg);
+        }
+
+        streamInfoArrCopy[streamNameIndex][1] = elemId;
       }
     }
 
-    readStream(streamName, newId);
+    readStream(streamInfoArrCopy);
   });
 }
 
