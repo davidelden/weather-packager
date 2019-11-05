4c34382d7be4d577ec0b771dd5eb3ae541d83f45
davidelden <deelden+github@gmail.com>
2019-11-05 11:56:55 -0600

    update index.js to read multiple streams

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const migrateLatest = require('./src/db/migrateLatest.js'),
       msgEmitter = require('./src/emitter/msgEmitter'),
       createWeatherPackage = require('./src/packager/createWeatherPackage.js'),
       updateWeatherPackage = require('./src/packager/updateWeatherPackage.js'),
-      startServer = require('./src/api/server.js'),
-      streamWeatherFetch = 'WeatherFetch',
-      streamWeatherSender = 'WeatherSender';
+      startServer = require('./src/api/server.js');
 
 migrateLatest();
-readStream(streamWeatherFetch);
-readStream(streamWeatherSender);
-msgEmitter.on('streamMessage', msg => createWeatherPackage(msg));
-msgEmitter.on('streamMessage', msg => updateWeatherPackage(msg));
+readStream([['WeatherFetch', '$'], ['WeatherSender', '$']]);
 startServer();
+
+msgEmitter.on('streamMessage', msg => {
+  createWeatherPackage(msg);
+  updateWeatherPackage(msg);
+});
